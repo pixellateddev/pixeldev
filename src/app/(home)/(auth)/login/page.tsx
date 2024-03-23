cf6ae81431ed801c64d954a7047010d9f30b8956
Himanshu Sagar <pixellateddev@proton.me>
2024-03-24 02:00:45 +0530

    fixed auth pages

--- src/app/(home)/(auth)/login/page.tsx
+++ src/app/(home)/(auth)/login/page.tsx
@@ -2,21 +2,25 @@ import { NextPage } from 'next'
 import classes from '../auth.module.scss'
 import Link from 'next/link'
 import { LoginForm } from '@/components/forms'
-import { Button } from 'antd'
+import { Button, Card } from 'antd'
 
 const Login: NextPage = async () => {
     return (
         <div className={classes.container}>
-            <div className={classes.formContainer}>
-                <h3 className={classes.formTitle}>Log in to Pixel Account</h3>
-                <LoginForm />
-                <p>
-                    Dont have an account?{' '}
-                    <Link href="/register">
-                        <Button type="link">Register here</Button>
-                    </Link>
-                </p>
-            </div>
+            <Card className={classes.card}>
+                <div className={classes.formContainer}>
+                    <h3 className={classes.formTitle}>
+                        Log in to Pixel Account
+                    </h3>
+                    <LoginForm />
+                    <p>
+                        Dont have an account?{' '}
+                        <Link href="/register">
+                            <Button type="link">Register here</Button>
+                        </Link>
+                    </p>
+                </div>
+            </Card>
         </div>
     )
 }
--- src/app/(home)/(auth)/register/page.tsx
+++ src/app/(home)/(auth)/register/page.tsx
@@ -2,21 +2,25 @@ import { NextPage } from 'next'
 import classes from '../auth.module.scss'
 import Link from 'next/link'
 import { RegisterForm } from '@/components/forms'
-import { Button } from 'antd'
+import { Button, Card } from 'antd'
 
 const Register: NextPage = async () => {
     return (
         <div className={classes.container}>
-            <div className={classes.formContainer}>
-                <h3 className={classes.formTitle}>Create a Pixel Account</h3>
-                <RegisterForm />
-                <p>
-                    Already have an account?{' '}
-                    <Link href="/login">
-                        <Button type="link">Login here</Button>
-                    </Link>
-                </p>
-            </div>
+            <Card className={classes.card}>
+                <div className={classes.formContainer}>
+                    <h3 className={classes.formTitle}>
+                        Create a Pixel Account
+                    </h3>
+                    <RegisterForm />
+                    <p>
+                        Already have an account?{' '}
+                        <Link href="/login">
+                            <Button type="link">Login here</Button>
+                        </Link>
+                    </p>
+                </div>
+            </Card>
         </div>
     )
 }
